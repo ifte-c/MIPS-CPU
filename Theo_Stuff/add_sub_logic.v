5b0b06f71b70d20c830e15c514dd5bb92eb6f006
theogam02 <94688075+theogam02@users.noreply.github.com>
2021-11-29 20:49:35 +0000

    Update add_sub_logic.v
    Add, Sub, And, Or, Xor functionality added. This will hypothetically output the results to all instructions that need an ALU output of Add, Sub, And, Or, Xor.

--- Theo_Stuff/add_sub_logic.v
+++ Theo_Stuff/add_sub_logic.v
@@ -6,3 +6,57 @@ module (
     output logic[31:0] add_sub_out
 );
 
+logic[31:0] add_res;
+logic[31:0] sub_res;
+logic[31:0] and_res;
+logic[31:0] or_res;
+logic[31:0] xor_res;
+
+assign add_res = op1 + op2;
+assign sub_res = op1 - op2;
+assign and_res = op1 & op2; 
+assign or_res = op1 | op2;
+assign xor_res = op1 ^ op2;
+
+
+always_comb begin
+    if (opcode == 000000) begin
+        
+     /* if (funct == 100001) begin
+            add_sub_out = add_res;
+        end
+        if (funct == 100010) begin
+            add_sub_out = sub_res;
+        end
+        if (funct == 100100) begin
+            add_sub_out = and_res;
+        end
+        if (funct == 100101) begin
+            add_sub_res = or_res;
+        end
+        if (funct == 100110) begin
+            add_sub_res = xor_res;*
+        end */
+
+        add_sub_out = funct == 100001 ? add_res : 
+        funct == 100010 ? sub_res :
+        funct == 100100 ? and_res :
+        funct == 100101 ? or_res :
+        funct == 100110 ? xor_res ;
+
+
+    end else begin
+
+        /*if (opcode == 001001 || 100000 || 100100 || 100001 || 100101 || 100011 || 101000 || 101001 || 101011) begin
+            add_sub_res = add_res;
+        end */
+
+        add_sub_res = (opcode == 001001 || 100000 || 100100 || 100001 || 100101 || 100011 || 101000 || 101001 || 101011) ? add_res:
+        opcode == 001100 ? and_res :
+        opcode == 001101 ? or_res : 
+        opcode == 001110 ? xor_res ;
+
+    end
+
+    
+end

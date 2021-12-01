7da7b711c25ee7bfde00cff5b67c826ccedc05b1
Ifte C <91498574+ifte-c@users.noreply.github.com>
2021-12-01 16:25:25 +0000

    Update mips_cpu_pc.v

--- Ifte_Stuff/mips_cpu_pc.v
+++ Ifte_Stuff/mips_cpu_pc.v
@@ -1,13 +1,34 @@
 module mips_cpu_pc(
     input logic clk,
-    input logic[31:0] addr,
-    input logic[31:0] instr,
-    output logic[31:0] addr_out
+    input logic[5:0] opcode,
+    input logic[5:0] funct,
+    input logic[4:0] rs,
+    input logic[15:0] offset,
+    input logic[25:0] target,
+    input logic[3:0] control,
+    output logic[31:0] pc_out,
+    output logic[4:0] rd,
+    output logic[31:0] r31
 );
 
+    logic[31:0] pc;
+    logic[31:0] pc_inc;
+
+    initial pc = 32'b10111111110000000000000000000000;
+    assign pc_inc = pc+4;
+
     always_ff @(posedge clk) begin
-        if (instr[31:26] == 000010) begin // this implements jump instruction maybe idek
-            addr <= { 4'b0000, instr[25:0], 2'b00 } + { (addr+4)[31:28], 28'b0000000000000000000000000000 };
+        if (opcode == 000010) begin // jump
+            pc_out <= { 4'b0000, target, 2'b00 } + { pc_inc[31:28], 28'b0000000000000000000000000000 };
+        end else if (opcode == 000011) begin //jal
+            r31 <= pc_inc;
+            pc_out <= { 4'b0000, target, 2'b00 };
+        end else if (opcode == 0 && funct == 6'b001001) begin //jalr
+            rd <= pc_inc; //wrong
+            pc_out <= rs; //wrong
+        end
+        else begin
+            pc <= pc_inc;
         end
     end
 

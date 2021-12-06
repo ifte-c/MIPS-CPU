bab07da878f9f2e25a33c3d248bdce56b1c4de43
Ifte C <91498574+ifte-c@users.noreply.github.com>
2021-12-06 15:43:22 +0000

    PC debug and testing

--- Ifte_Stuff/mips_cpu_pc.v
+++ Ifte_Stuff/mips_cpu_pc.v
@@ -5,6 +5,7 @@ module mips_cpu_pc(
     input logic[5:0] funct,
     input logic[4:0] rt,
     input logic[4:0] sa,
+    input logic[4:0] rd,
     input logic[31:0] rs_data, //received from register
     input logic[15:0] offset,
     input logic[25:0] target,
@@ -13,56 +14,65 @@ module mips_cpu_pc(
     output logic[31:0] regstore
 );
 
-    logic[31:0] pc = 32'hBFC00000;
-    logic[31:0] balval;
-    logic delflag = 0;
+    logic[31:0] dest;
+    logic delayflag = 0;
+    logic[31:0] pc_inc;
+
+    assign pc_inc = pc + 4;
 
     always_ff @(posedge clk) begin
-        if (opcode == 000010) begin                                                     //j
-            balval <= { 4'b0000, target, 2'b00 } + { pc_inc[31:28], 28'b0000000000000000000000000000 };
-            pc <= pc + 4;
-            delflag <= 1;
-        end else if (opcode == 000011) begin                                            //jal
-            regstore <= pc + 8; //goes to r31
-            balval <= { 4'b0000, target, 2'b00 } + { pc_inc[31:28], 28'b0000000000000000000000000000 };
-            pc <= pc + 4;
-            delflag <= 1;
-        end else if (opcode == 0 && funct == 6'b001001 && rt == 0 && sa == 0) begin     //jalr
-            regstore <= pc + 8; //goes to rd
-            balval <= rs_data;
-            pc <= pc + 4;
-            delflag <= 1;
-        end else if (
-        opcode == 0 && funct == 6'b001000 && rt == 0 && rd == 0 && sa == 0              //jr
-        ) begin
-            balval <= rs_data;
-            pc <= pc + 4;
-            delflag <= 1;
-        end else if (
-        opcode == 6'b000100 && control[3] == 1 ||                                       //beq
-        opcode == 6'b000101 && control[3] != 1 ||                                       //bne
-        opcode == 6'b000111 && rt == 5'b00000 && control[2] == 1 ||                     //bgtz
-        opcode == 6'b000001 && rt == 5'b00000 && control[0] == 1 ||                     //bltz
-        opcode == 6'b000000 && rt == 5'b00001 && (control[1] == 1 || control[2] == 1) ||//bgez
-        opcode == 6'b000000 && rt == 5'b00000 && (control[1] == 1 || control[0] == 1)   //blez
-        ) begin
-            balval <= pc + 4 + {14{offset[15]}, offset, 2'b00};
-            pc <= pc + 4;
-            delflag <= 1;
-        end else if (
-        opcode == 6'b000001 && rt == 5'b10001 && (control[1] == 1 || control[2] == 1) ||//bgezal
-        opcode == 6'b000000 && rt == 5'b00001 && control[0] == 1                        //bltzal
-        ) begin
-            regstore <= pc + 8; //goes to r31
-            balval <= pc + 4 + {14{offset[15]}, offset, 2'b00};
-            pc <= pc + 4;
-            delflag <= 1;
+        if (reset == 1) begin
+            pc <= 0;
+            dest <= 0;
+            delayflag <= 0;
+            regstore <= 0;
         end else begin
-            if (delflag == 1) begin
-                pc <= balval;
-                delflag <= 0;
-            end else begin
+            if (opcode == 000010) begin                                                     //j
+                dest <= { 4'b0000, target, 2'b00 } + { pc_inc[31:28], 28'b0000000000000000000000000000 };
                 pc <= pc + 4;
+                delayflag <= 1;
+            end else if (opcode == 000011) begin                                            //jal
+                regstore <= pc + 8; //goes to r31
+                dest <= { 4'b0000, target, 2'b00 } + { pc_inc[31:28], 28'b0000000000000000000000000000 };
+                pc <= pc + 4;
+                delayflag <= 1;
+            end else if (opcode == 0 && funct == 6'b001001 && rt == 0 && sa == 0) begin     //jalr
+                regstore <= pc + 8; //goes to rd
+                dest <= rs_data;
+                pc <= pc + 4;
+                delayflag <= 1;
+            end else if (
+            opcode == 0 && funct == 6'b001000 && rt == 0 && rd == 0 && sa == 0              //jr
+            ) begin
+                dest <= rs_data;
+                pc <= pc + 4;
+                delayflag <= 1;
+            end else if (
+            opcode == 6'b000100 && control[3] == 1 ||                                       //beq
+            opcode == 6'b000101 && control[3] != 1 ||                                       //bne
+            opcode == 6'b000111 && rt == 5'b00000 && control[2] == 1 ||                     //bgtz
+            opcode == 6'b000001 && rt == 5'b00000 && control[0] == 1 ||                     //bltz
+            opcode == 6'b000000 && rt == 5'b00001 && (control[1] == 1 || control[2] == 1) ||//bgez
+            opcode == 6'b000000 && rt == 5'b00000 && (control[1] == 1 || control[0] == 1)   //blez
+            ) begin
+                dest <= pc + 4 + { {14{ offset[15] }}, offset, 2'b00 };
+                pc <= pc + 4;
+                delayflag <= 1;
+            end else if (
+            opcode == 6'b000001 && rt == 5'b10001 && (control[1] == 1 || control[2] == 1) ||//bgezal
+            opcode == 6'b000000 && rt == 5'b00001 && control[0] == 1                        //bltzal
+            ) begin
+                regstore <= pc + 8; //goes to r31
+                dest <= pc + 4 + { {14{ offset[15] }}, offset, 2'b00 };
+                pc <= pc + 4;
+                delayflag <= 1;
+            end else begin
+                if (delayflag == 1) begin
+                    pc <= dest;
+                    delayflag <= 0;
+                end else begin
+                    pc <= pc + 4;
+                end
             end
         end
     end
---
+++ Ifte_Stuff/pc_tb.v
@@ -0,0 +1,67 @@
+module pc_tb();
+
+    logic clk;
+    logic reset;
+    logic[5:0] opcode;
+    logic[5:0] funct;
+    logic[4:0] rt;
+    logic[4:0] sa;
+    logic[4:0] rd;
+    logic[31:0] rs_data; //received from register
+    logic[15:0] offset;
+    logic[25:0] target;
+    logic[3:0] control; // rs==rt | rs > 0 | rs == 0 | rs < 0
+    logic[31:0] pc; //10111111110000000000000000000000
+    logic[31:0] regstore;
+
+    initial begin
+        $dumpfile("pc_tb.vcd");
+        $dumpvars(0, pc_tb);
+
+        clk = 0;
+        repeat(1000) begin
+            #10;
+            clk = !clk;
+            #10;
+            clk = !clk;
+        end
+    end
+
+    initial begin
+        #5;
+        $display("Initial PC value: %b", pc);
+        opcode = 6'b000010;
+        target = 26'b01010101010111111111010101;
+
+        #10;
+        $display("PC value: %b", pc);
+        opcode = 0;
+        sa = 0;
+        funct = 6'b100000;
+
+        #10;
+        #10;
+        $display("PC value: %b", pc);
+
+        #10;
+        #10;
+        $display("PC value: %b", pc);
+    end
+
+    mips_cpu_pc dut(
+        .clk(clk),
+        .reset(reset),
+        .opcode(opcode),
+        .funct(funct),
+        .rt(rt),
+        .sa(sa),
+        .rd(rd),
+        .rs_data(rs_data),
+        .offset(offset),
+        .target(target),
+        .control(control),
+        .pc(pc),
+        .regstore(regstore)
+    );
+
+endmodule

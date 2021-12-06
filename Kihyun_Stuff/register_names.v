f628f312ea295675550800c19925e6b5f47f407a
Kihyun11 <yoo07220@gmail.com>
2021-12-06 13:02:27 +0000

    Create register_names.v

---
+++ Kihyun_Stuff/register_names.v
@@ -0,0 +1,34 @@
+names = [
+    ["$r0", "$zero"], // always 0
+    ["$r1", "$at"], // Reserved for assembler
+    ["$r2", "$v0"], // Stores results
+    ["$r3", "$v1"], 
+    ["$r4", "$a0"], // Stores arguments
+    ["$r5", "$a1"], 
+    ["$r6", "$a2"],
+    ["$r7", "$a3"],
+    ["$r8", "$t0"], // Temporaries, not saved
+    ["$r9", "$t1"],
+    ["$r10", "$t2"],
+    ["$r11", "$t3"],
+    ["$r12", "$t4"],
+    ["$r13", "$t5"],
+    ["$r14", "$t6"],
+    ["$r15", "$t7"],
+    ["$r16", "$s0"], // Contents saved for use later
+    ["$r17", "$s1"],
+    ["$r18", "$s2"],
+    ["$r19", "$s3"],
+    ["$r20", "$s4"],
+    ["$r21", "$s5"],
+    ["$r22", "$s6"],
+    ["$r23", "$s7"],
+    ["$r24", "$t8"], // More temporaries, not saved
+    ["$r25", "$t9"],
+    ["$r26", "$k0"], // Reserved by operating system
+    ["$r27", "$k1"],
+    ["$r28", "$gp"], // Global pointer
+    ["$r29", "$sp"], // Stack pointer
+    ["$r30", "$fp"], // Frame pointer
+    ["$r31", "$ra"], // Return address
+]
